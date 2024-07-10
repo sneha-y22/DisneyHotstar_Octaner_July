6ee8879f466f5747dc84e340024514f3ac9e1dde
Sneha Yadav <140615190+sneha-y22@users.noreply.github.com>
2024-07-10 18:56:56 +0530

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,101 @@
+let movies = [
+    {
+      image: "./Images/slider 2.PNG"
+    },
+    {
+      image: "./Images/slider 6.jpeg"
+    },
+    {
+      image: "./Images/slider 1.PNG"
+    },
+    {
+      image: "./Images/slider 3.PNG"
+    },
+    {
+      image: "./Images/slider 4.PNG"
+    },
+    {
+      image: "./Images/slider 5.PNG"
+    }
+  ];
+  
+  const carousel = document.querySelector(".carousel");
+  let sliders = [];
+  
+  let slideIndex = 0; 
+  
+  const createSlide = () => {
+    if (slideIndex >= movies.length) {
+      slideIndex = 0;
+    }
+
+    let slide = document.createElement("div");
+    let imgElement = document.createElement("img");
+    let content = document.createElement("div");
+    let h1 = document.createElement("h1");
+    let p = document.createElement("p");
+  
+    imgElement.appendChild(document.createTextNode(""));
+    
+    content.appendChild(h1);
+    content.appendChild(p);
+    slide.appendChild(content);
+    slide.appendChild(imgElement);
+    carousel.appendChild(slide);
+  
+    imgElement.src = movies[slideIndex].image;
+    slideIndex++;
+  
+    slide.className = "slider";
+    content.className = "slide-content";
+    
+    sliders.push(slide);
+  
+    if (sliders.length) {
+      sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
+        30 * (sliders.length - 2)
+      }px)`;
+    }
+  };
+  
+  for (let i = 0; i < 3; i++) {
+    createSlide();
+  }
+  
+  setInterval(() => {
+    createSlide();
+  }, 3000);
+  
+  /// video cards
+  
+  const videoCards = [...document.querySelectorAll(".video-card")];
+  
+  videoCards.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      let video = item.children[1];
+      video.play();
+    });
+    item.addEventListener("mouseleave", () => {
+      let video = item.children[1];
+      video.pause();
+    });
+  });
+  
+  // card sliders
+  
+  let cardContainers = [...document.querySelectorAll(".card-container")];
+  let preBtns = [...document.querySelectorAll(".pre-btn")];
+  let nxtBtns = [...document.querySelectorAll(".nxt-btn")];
+  
+  cardContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+  
+    nxtBtns[i].addEventListener("click", () => {
+      item.scrollLeft += containerWidth - 200;
+    });
+  
+    preBtns[i].addEventListener("click", () => {
+      item.scrollLeft -= containerWidth + 200;
+    });
+  });
